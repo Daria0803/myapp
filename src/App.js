052c429be8ba88d479077224ef2ee7db7c8f3fe6
Daria0803 <64463296+Daria0803@users.noreply.github.com>
2021-03-20 23:22:50 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import "./App.css";
+//import SearchField from 'react-search-field';
+
+const fields = [
+    {name: "key", zip: "*%3A*"},
+];
+
+class SolrDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      solrData: null
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    //const cors = require("cors")
+    //app.use(cors())
+    const URL = "http://localhost:8983/solr/test/select?q=*%3A*&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    //fetch(URL).then(response => response.json())
+    //.then(result => {this.setState({ solrData: result });
+    //});
+    fetch(URL)
+        .then(response => response.json())
+        .then(solrData => this.setState({ solrData }));
+  }
+  render() {
+    //const solrData = this.state;
+    const solrData = this.state.solrData;
+    if (!solrData) return <div>Loading</div>;
+    return <div>{JSON.stringify(solrData)}</div>;
+    //return <h1>{solrData.goals[0].gs_id}</h1>;
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    };
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+     <div className="App">
+    {fields.map((keyword, index) => (
+      <button
+        key={index}
+        onClick={() => {
+          this.setState({ activePlace: index});
+        }}
+      >
+          {keyword.name}
+      </button>
+    ))}
+    <SolrDisplay
+          key={activePlace}
+          zip={fields[activePlace].zip}
+    />
+  </div>
+);
+  }
+}
+
+export default App;
